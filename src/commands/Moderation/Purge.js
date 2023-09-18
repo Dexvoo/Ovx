518108a1a352eab31678c794ba0c7a1f221457d5
Dexvo <63011374+Dexvoo@users.noreply.github.com>
2023-09-18 16:31:22 +0100

    Purge Command

---
+++ src/commands/Moderation/Purge.js
@@ -0,0 +1,100 @@
+const {
+	SlashCommandBuilder,
+	PermissionFlagsBits,
+	EmbedBuilder,
+	CommandInteraction,
+} = require('discord.js');
+
+const { sendEmbed, sendErrorEmbed } = require('../../utils/Embeds.js');
+const { guildCheck, permissionCheck } = require('../../utils/Checks.js');
+const { sleep } = require('../../utils/ConsoleLogs.js');
+require('dotenv').config();
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('purge')
+		.setDescription('Delete a certain amount of messages.')
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+		.setDMPermission(false)
+		.addIntegerOption((option) =>
+			option
+				.setName('amount')
+				.setDescription('The amount of messages you would like to delete.')
+				.setRequired(true)
+		),
+	/**
+	 *
+	 * @param {CommandInteraction} interaction
+	 * @returns
+	 */
+
+	async execute(interaction) {
+		try {
+			// Deconstructing interaction
+			const { guild, member, options, user, client, channel } = interaction;
+
+			// Checking if the user is in a guild
+			if (!(await guildCheck(interaction))) return;
+
+			// Bot permissions
+			const botPermissionsArry = ['ManageNicknames'];
+			const botPermissions = await permissionCheck(
+				interaction,
+				botPermissionsArry,
+				client
+			);
+
+			if (!botPermissions[0])
+				return await sendEmbed(
+					interaction,
+					`Bot Missing Permissions: \`${botPermissions[1]}\``
+				);
+
+			// User permissions
+			const userPermissionsArry = ['ManageNicknames'];
+			const userPermissions = await permissionCheck(
+				interaction,
+				userPermissionsArry,
+				member
+			);
+
+			if (!userPermissions[0])
+				return await sendEmbed(
+					interaction,
+					`User Missing Permissions: \`${userPermissions[1]}\``
+				);
+
+			await sendEmbed(interaction, 'Attempting to change nickname');
+			await sleep(2000);
+
+			// Variables
+			const amount = options.getInteger('amount');
+
+			// Checking if the amount is valid
+			if (amount < 1 || amount > 100)
+				return await sendEmbed(
+					interaction,
+					'Please provide a number between 1 and 100'
+				);
+
+			// Deleting messages
+			const messagesDeleted = await channel
+				.bulkDelete(amount, true)
+				.catch((error) => {
+					console.error(error);
+					return sendErrorEmbed(interaction, error);
+				});
+
+			// Sending embed
+			await sendEmbed(interaction, `Deleted ${messagesDeleted.size} messages`);
+		} catch (error) {
+			console.error(error);
+			await sendErrorEmbed(interaction, error);
+			await sendEmbed(
+				interaction,
+				`There was an error running this command\n\n${error}`
+			);
+			return;
+		}
+	},
+};
