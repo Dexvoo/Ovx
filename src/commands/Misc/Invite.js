8a07cd06f8111232cbc3f98bb8a415a81973102f
Dexvo <63011374+Dexvoo@users.noreply.github.com>
2023-09-25 00:05:42 +0100

    Avatar | Invite | Translate Commands

---
+++ src/commands/Misc/Invite.js
@@ -0,0 +1,90 @@
+const {
+	SlashCommandBuilder,
+	EmbedBuilder,
+	PermissionFlagsBits,
+	parseEmoji,
+	ButtonBuilder,
+	ButtonStyle,
+	ActionRowBuilder,
+} = require('discord.js');
+const { sendEmbed, sendErrorEmbed } = require('../../utils/Embeds.js');
+const { sleep } = require('../../utils/ConsoleLogs.js');
+const {
+	DeveloperMode,
+	PrivateToken,
+	PublicToken,
+	EmbedColour,
+	FooterImage,
+	FooterText,
+} = process.env;
+require('dotenv').config();
+
+module.exports = {
+	cooldown: 5,
+	data: new SlashCommandBuilder()
+		.setName('invite')
+		.setDescription(
+			'Generate a invite to the support server or the bot to your server.'
+		)
+		.setDMPermission(true),
+
+	/**
+	 * @param {CommandInteraction} interaction
+	 * @returns
+	 */
+
+	async execute(interaction) {
+		// Deconstructing interaction
+		const { client } = interaction;
+		const { id: ClientID } = client.user;
+
+		await sendEmbed(interaction, 'Generating Invite');
+		await sleep(2000);
+
+		// Variables
+
+		// Buttons
+		var LinkButton;
+		if (DeveloperMode == 'true') {
+			LinkButton = new ActionRowBuilder().addComponents(
+				new ButtonBuilder()
+					.setStyle(ButtonStyle.Link)
+					.setLabel('Support Server')
+					.setURL('https://discord.gg/uPGkcXyNZ3'),
+				new ButtonBuilder()
+					.setStyle(ButtonStyle.Link)
+					.setLabel('Invite Bot')
+					.setURL(
+						`https://discord.com/api/oauth2/authorize?client_id=${ClientID}&permissions=9896812882982&scope=bot`
+					)
+					.setDisabled(true)
+			);
+		} else {
+			LinkButton = new ActionRowBuilder().addComponents(
+				new ButtonBuilder()
+					.setStyle(ButtonStyle.Link)
+					.setLabel('Support Server')
+					.setURL('https://discord.gg/uPGkcXyNZ3'),
+				new ButtonBuilder()
+					.setStyle(ButtonStyle.Link)
+					.setLabel('Invite Bot')
+					.setURL(
+						`https://discord.com/api/oauth2/authorize?client_id=${ClientID}&permissions=9896812882982&scope=bot`
+					)
+			);
+		}
+
+		// Embed
+		const InviteEmbed = new EmbedBuilder()
+			.setColor(EmbedColour)
+			.setDescription(
+				'• Click the buttons below to join the support server or invite the bot to your server! •'
+			)
+			.setTimestamp()
+			.setFooter({ text: FooterText, iconURL: FooterImage });
+		interaction.editReply({
+			embeds: [InviteEmbed],
+			components: [LinkButton],
+		});
+	},
+};
---
+++ src/commands/Misc/Translate.js
@@ -0,0 +1,113 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { FooterText, FooterImage, EmbedColour } = process.env;
+const translate = require('@iamtraction/google-translate');
+const { sendEmbed, sendErrorEmbed } = require('../../utils/Embeds.js');
+const { sleep } = require('../../utils/ConsoleLogs.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('translate')
+		.setDescription('Translate text to another language.')
+		.addStringOption((option) =>
+			option
+				.setName('text')
+				.setDescription('Text to translate')
+				.setRequired(true)
+		)
+		.addStringOption((option) =>
+			option
+				.setName('language')
+				.setDescription('Language to translate to')
+				.addChoices(
+					{
+						name: 'English',
+						value: 'en',
+					},
+					{
+						name: 'Spanish',
+						value: 'es',
+					},
+					{
+						name: 'French',
+						value: 'fr',
+					},
+					{
+						name: 'German',
+						value: 'de',
+					},
+					{
+						name: 'Italian',
+						value: 'it',
+					},
+					{
+						name: 'Japanese',
+						value: 'ja',
+					},
+					{
+						name: 'Korean',
+						value: 'ko',
+					},
+					{
+						name: 'Portuguese',
+						value: 'pt',
+					},
+					{
+						name: 'Russian',
+						value: 'ru',
+					},
+					{
+						name: 'Arabic',
+						value: 'ar',
+					}
+				)
+				.setRequired(true)
+		),
+	async execute(interaction) {
+		const { member, options, user, client } = interaction;
+
+		// Placeholder Embed
+		await sendEmbed(interaction, `Translating Text`);
+		await sleep(2000);
+
+		// Variables
+		const text = options.getString('text');
+		const language = options.getString('language');
+		const translation = await translate(text, { to: language });
+		const LanguageList = {
+			en: 'English',
+			es: 'Spanish',
+			fr: 'French',
+			de: 'German',
+			it: 'Italian',
+			ja: 'Japanese',
+			ko: 'Korean',
+			pt: 'Portuguese',
+			ru: 'Russian',
+			ar: 'Arabic',
+		};
+		var LanguageFrom = LanguageList[translation.from.language.iso];
+		var Languageto = LanguageList[language];
+
+		const TranslationEmbed = new EmbedBuilder()
+			.setColor(EmbedColour)
+			.setTitle('Translate')
+			.addFields(
+				{
+					name: 'Author',
+					value: `@${user.username} (${user})`,
+				},
+				{
+					name: `Original Text : ${LanguageFrom}`,
+					value: text,
+				},
+				{
+					name: `Translated Text : ${Languageto}`,
+					value: translation.text,
+				}
+			)
+			.setTimestamp()
+			.setFooter({ text: FooterText, iconURL: FooterImage });
+
+		interaction.editReply({ embeds: [TranslationEmbed] });
+	},
+};
