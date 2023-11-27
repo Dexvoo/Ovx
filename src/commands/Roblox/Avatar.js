47f40c6d0987b3743bae8403e72f3a986a7e7eb7
Dexvo <63011374+Dexvoo@users.noreply.github.com>
2023-11-27 14:23:51 +0000

    Update Avatar.js

--- src/commands/Roblox/Avatar.js
+++ src/commands/Roblox/Avatar.js
@@ -252,120 +252,130 @@ module.exports = {
 						interaction,
 						`Please specify a valid username.`
 					);
-				await noblox.getIdFromUsername(userInput).then(async (id) => {
-					// ✅
-					if (id) {
-						await noblox.getPlayerInfo(parseInt(id)).then(async (info) => {
-							// ✅
-							let imageUrl;
+				await noblox
+					.getIdFromUsername(userInput)
+					.then(async (id) => {
+						// ✅
+						if (id) {
+							await noblox
+								.getPlayerInfo(parseInt(id))
+								.then(async (info) => {
+									// ✅
+									let imageUrl;
 
-							let thumbnail_circHeadshot = await noblox.getPlayerThumbnail(
-								// ✅
-								id,
-								420,
-								'png',
-								true,
-								'Body'
-							);
+									let thumbnail_circHeadshot = await noblox.getPlayerThumbnail(
+										// ✅
+										id,
+										420,
+										'png',
+										true,
+										'Body'
+									);
 
-							if (
-								Array.isArray(thumbnail_circHeadshot) &&
-								thumbnail_circHeadshot.length > 0
-							) {
-								imageUrl = thumbnail_circHeadshot[0].imageUrl;
-							}
-
-							const avatar = await noblox.getAvatar(id); // ✅
-
-							const RobloxSearchEmbed = new EmbedBuilder()
-								.setURL(`https://roblox.com/users/${id}/profile`)
-								.setTitle(`@${info.username} Avatar`)
-								.setImage(imageUrl)
-								.addFields(
-									{
-										name: 'Scales',
-										value: `Height: ${avatar.scales.height}\nWidth: ${avatar.scales.width}\nHead: ${avatar.scales.head}\nDepth: ${avatar.scales.depth}\nProportion: ${avatar.scales.proportion}`,
-										inline: true,
-									},
-									{
-										name: 'Body Colors',
-										value: `Head: ${avatar.bodyColors.headColorId}\nTorso:${avatar.bodyColors.torsoColorId}\nRight Arm:${avatar.bodyColors.rightArmColorId}\nRight Arm:${avatar.bodyColors.leftArmColorId}\nLeft Leg:${avatar.bodyColors.rightLegColorId}\nRight Leg:${avatar.bodyColors.leftLegColorId}`,
-										inline: true,
+									if (
+										Array.isArray(thumbnail_circHeadshot) &&
+										thumbnail_circHeadshot.length > 0
+									) {
+										imageUrl = thumbnail_circHeadshot[0].imageUrl;
 									}
-								)
+
+									const avatar = await noblox.getAvatar(id); // ✅
+
+									const RobloxSearchEmbed = new EmbedBuilder()
+										.setURL(`https://roblox.com/users/${id}/profile`)
+										.setTitle(`@${info.username} Avatar`)
+										.setImage(imageUrl)
+										.addFields(
+											{
+												name: 'Scales',
+												value: `Height: ${avatar.scales.height}\nWidth: ${avatar.scales.width}\nHead: ${avatar.scales.head}\nDepth: ${avatar.scales.depth}\nProportion: ${avatar.scales.proportion}`,
+												inline: true,
+											},
+											{
+												name: 'Body Colors',
+												value: `Head: ${avatar.bodyColors.headColorId}\nTorso:${avatar.bodyColors.torsoColorId}\nRight Arm:${avatar.bodyColors.rightArmColorId}\nRight Arm:${avatar.bodyColors.leftArmColorId}\nLeft Leg:${avatar.bodyColors.rightLegColorId}\nRight Leg:${avatar.bodyColors.leftLegColorId}`,
+												inline: true,
+											}
+										)
+										.setTimestamp()
+										.setFooter({ text: FooterText, iconURL: FooterImage });
+
+									// List assest from avatar and check for types
+									var assetList = avatar.assets;
+									const accessoryUrls = [];
+									const accessoryNames = [];
+									const accessoryTypes = [];
+									var shirtName;
+									var shirtUrl;
+									var pantsName;
+									var pantsUrl;
+									assetList.map((asset) => {
+										if (asset.assetType.name === 'Shirt') {
+											shirtName = asset.name;
+											shirtUrl = `https://www.roblox.com/catalog/${asset.id}`;
+										}
+
+										if (asset.assetType.name === 'Pants') {
+											pantsName = asset.name;
+											pantsUrl = `https://www.roblox.com/catalog/${asset.id}`;
+										}
+
+										if (asset.assetType.name.includes('Accessory')) {
+											accessoryNames.push(asset.name);
+											accessoryTypes.push(asset.assetType.name);
+											accessoryUrls.push(
+												`https://www.roblox.com/catalog/${asset.id}`
+											);
+										}
+									});
+
+									// Get Accessories
+									var accessoriesList = '';
+									for (let i = 0; i < accessoryNames.length; i++) {
+										accessoriesList += `${accessoryTypes[i].replace(
+											'Accessory',
+											''
+										)}: [${accessoryNames[i]}](${accessoryUrls[i]})\n`;
+									}
+
+									if (shirtName === undefined) {
+										shirtName = 'No Shirt';
+										shirtUrl = 'https://www.roblox.com/catalog/0';
+									}
+
+									if (pantsName === undefined) {
+										pantsName = 'No Pants';
+										pantsUrl = 'https://www.roblox.com/catalog/0';
+									}
+
+									RobloxSearchEmbed.addFields({
+										name: 'Assets',
+										value: `Shirt: [${shirtName}](${shirtUrl})\nPants: [${pantsName}](${pantsUrl})\n${accessoriesList}`,
+										inline: false,
+									});
+
+									interaction.editReply({
+										embeds: [RobloxSearchEmbed],
+									});
+								})
+								.catch((err) => {
+									console.log(err);
+								});
+						} else {
+							const RobloxSearchEmbed = new EmbedBuilder()
+								.setTitle('Roblox Avatar')
+								.setDescription('No User Found')
+								.setColor('Red')
 								.setTimestamp()
 								.setFooter({ text: FooterText, iconURL: FooterImage });
-
-							// List assest from avatar and check for types
-							var assetList = avatar.assets;
-							const accessoryUrls = [];
-							const accessoryNames = [];
-							const accessoryTypes = [];
-							var shirtName;
-							var shirtUrl;
-							var pantsName;
-							var pantsUrl;
-							assetList.map((asset) => {
-								if (asset.assetType.name === 'Shirt') {
-									shirtName = asset.name;
-									shirtUrl = `https://www.roblox.com/catalog/${asset.id}`;
-								}
-
-								if (asset.assetType.name === 'Pants') {
-									pantsName = asset.name;
-									pantsUrl = `https://www.roblox.com/catalog/${asset.id}`;
-								}
-
-								if (asset.assetType.name.includes('Accessory')) {
-									accessoryNames.push(asset.name);
-									accessoryTypes.push(asset.assetType.name);
-									accessoryUrls.push(
-										`https://www.roblox.com/catalog/${asset.id}`
-									);
-								}
-							});
-
-							// Get Accessories
-							var accessoriesList = '';
-							for (let i = 0; i < accessoryNames.length; i++) {
-								accessoriesList += `${accessoryTypes[i].replace(
-									'Accessory',
-									''
-								)}: [${accessoryNames[i]}](${accessoryUrls[i]})\n`;
-							}
-
-							if (shirtName === undefined) {
-								shirtName = 'No Shirt';
-								shirtUrl = 'https://www.roblox.com/catalog/0';
-							}
-
-							if (pantsName === undefined) {
-								pantsName = 'No Pants';
-								pantsUrl = 'https://www.roblox.com/catalog/0';
-							}
-
-							RobloxSearchEmbed.addFields({
-								name: 'Assets',
-								value: `Shirt: [${shirtName}](${shirtUrl})\nPants: [${pantsName}](${pantsUrl})\n${accessoriesList}`,
-								inline: false,
-							});
-
 							interaction.editReply({
 								embeds: [RobloxSearchEmbed],
 							});
-						});
-					} else {
-						const RobloxSearchEmbed = new EmbedBuilder()
-							.setTitle('Roblox Avatar')
-							.setDescription('No User Found')
-							.setColor('Red')
-							.setTimestamp()
-							.setFooter({ text: FooterText, iconURL: FooterImage });
-						interaction.editReply({
-							embeds: [RobloxSearchEmbed],
-						});
-					}
-				});
+						}
+					})
+					.catch((err) => {
+						console.log(err);
+					});
 				break;
 		}
 	},
