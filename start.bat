7add05a1ef83291e26e6459787c5137e8d726b80
Dexvo <63011374+Dexvoo@users.noreply.github.com>
2023-12-01 12:10:04 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 
 start cmd /k pm2 monit
-pm2 start src/index.js --name "Oxv" --watch --ignore-watch="node_modules"
+pm2 start src/index.js --name "Oxv" --watch --ignore-watch="node_modules", "out", "videos", "package.json", "package-lock.json", ".git", "Workspace"
