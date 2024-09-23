86b636dc289be231cb2ee7c314146beb3478dd54
Dexvo <63011374+Dexvoo@users.noreply.github.com>
2024-09-23 01:08:34 +0100

    /rps command

--- src/commands/Games/CoinFlip.js
+++ src/commands/Games/CoinFlip.js
@@ -1,10 +1,24 @@
-const { SlashCommandBuilder, EmbedBuilder, CommandInteraction, InteractionContextType, PermissionFlagsBits, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, CommandInteraction, InteractionContextType, PermissionFlagsBits, ButtonBuilder, ButtonStyle, ActionRowBuilder, User } = require('discord.js');
 const { UserCurrency } = require('../../models/UserCurrency');
 
 const choices = [
     { name: 'Heads', beats: 'Tails', emoji: '<:OVX_Tails:1286316509525704766>' },
     { name: 'Tails', beats: 'Heads', emoji: '<:OVX_Heads:1286316499446796400>' },
 ]
+
+const messages = {
+    InvalidAmount: 'The amount must be greater than 0',
+    UserNotEnoughCash: 'You don\'t have enough cash',
+    GameOver: 'Game over, {user} did not respond in time',
+    BotChallenge: 'You have challenged the bot to a coinflip for {amount} cash!',
+    BotWin: `You lost {amount} cash!\n\nYou : {choice} \nBot : {botChoice}`,
+    BotLose: `You won {amount} cash!\n\nYou : {choice} \nBot : {botChoice}`,
+    UserNoBot: `You can't bet against a bot`,
+    UserNoSelf: `You can't bet against yourself`,
+    TargetNotEnoughCash: `The target user doesn't have enough cash`,
+    UserChallenge: `{user} has challenged {target} to a coinflip for {amount} cash!`,
+    UserWin: `{winner} won {amount} cash!\n\n{choice} \n{targetChoice}`,
+}
 
 module.exports = {
     cooldown: 5,
@@ -14,7 +28,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('coinflip')
         .setDescription('Flip a coin')
-        .setContexts( InteractionContextType.Guild )
+        .setContexts( InteractionContextType.Guild, InteractionContextType.PrivateChannel )
         .addIntegerOption(option => option
             .setName('amount')
             .setDescription('The amount of cash you want to bet')
@@ -60,49 +74,85 @@ module.exports = {
         if(amount < 1) {
             const Embed = new EmbedBuilder()
                 .setColor('Red')
-                .setDescription(`The amount must be greater than 0`);
+                .setDescription(messages.InvalidAmount);
             return await interaction.reply({ embeds: [Embed], ephemeral: true });
         }
 
         if(userCurrency.cash < amount) {
             const Embed = new EmbedBuilder()
                 .setColor('Red')
-                .setDescription(`You don't have enough cash`);
+                .setDescription(messages.UserNotEnoughCash);
             return await interaction.reply({ embeds: [Embed], ephemeral: true });
         }
 
         if(!targetUser) {
             // bet against the bot
-            const randomNumber = Math.floor(Math.random() * (2 - 0) + 0);
-            const sides = ['heads', 'tails'];
+            const StartGameEmbed = new EmbedBuilder()
+                .setColor('Blurple')
+                .setDescription(messages.BotChallenge.replace('{amount}', amount.toLocaleString()));
 
-            if(sides[randomNumber] === side) {
+            const Buttons = choices.map(choice => {
+                return new ButtonBuilder()
+                    .setCustomId(`Ovx_Coinflip_${choice.name}`)
+                    .setLabel(`${choice.name}`)
+                    .setEmoji(choice.emoji)
+                    .setStyle(ButtonStyle.Primary)
+            });
 
-                userCurrency.cash += amount;
-                await userCurrency.save();
+            const row = new ActionRowBuilder().addComponents(Buttons);
 
-                const Embed = new EmbedBuilder()
-                    .setColor('Blurple')
-                    .setDescription(`You won ${amount} cash!`);
-                return await interaction.reply({ embeds: [Embed] });
-            } else {
+            const reply = await interaction.reply({ embeds: [StartGameEmbed], components: [row] });
 
-                userCurrency.cash -= amount;
-                await userCurrency.save();
-
+            const userInteraction = await reply.awaitMessageComponent({ filter: i => i.user.id === member.id, time: 60_000 }).catch(async () => {
                 const Embed = new EmbedBuilder()
                     .setColor('Red')
-                    .setDescription(`You lost ${amount} cash!`);
-                return await interaction.reply({ embeds: [Embed] });
-            }
+                    .setDescription(messages.GameOver.replace('{user}', user.username));
+                    await reply.edit({ embeds: [Embed], components: [] }).catch(() => {});
+                return
+            })
+    
+            if(!userInteraction) return;
+
+            const userChoice = choices.find(choice => userInteraction.customId === `Ovx_Coinflip_${choice.name}`);
+            const botChoice = choices.find(choice => userChoice.beats === choice.name);
+
+            const randomNumber = Math.floor(Math.random() * (2 - 0) + 0);
+            const sides = ['Heads', 'Tails'];
+
+            if(sides[randomNumber] === userChoice.name) {
+                    
+                    userCurrency.cash += amount;
+                    await userCurrency.save();
+    
+                    const Embed = new EmbedBuilder()
+                        .setColor('Blurple')
+                        .setDescription(messages.BotLose.replace('{amount}', amount.toLocaleString()).replace('{choice}', `${userChoice.name} ${userChoice.emoji}`).replace('{botChoice}',  `${botChoice.name} ${botChoice.emoji}`));
+                    await reply.edit({ embeds: [Embed], components: [] }).catch(() => {});
+                } else {
+                    
+                    userCurrency.cash -= amount;
+                    await userCurrency.save();
+        
+                    const Embed = new EmbedBuilder()
+                        .setColor('Red')
+                        .setDescription(messages.BotWin.replace('{amount}', amount.toLocaleString()).replace('{choice}', `${userChoice.name} ${userChoice.emoji}`).replace('{botChoice}',  `${botChoice.name} ${botChoice.emoji}`));
+                    await reply.edit({ embeds: [Embed], components: [] }).catch(() => {});
+                }
+
+            return;
         }
 
+        if(targetUser.bot) {
+            const Embed = new EmbedBuilder()
+                .setColor('Red')
+                .setDescription(messages.UserNoBot);
+            return await interaction.reply({ embeds: [Embed], ephemeral: true });   
+        }
 
-        // bet against another user
         if(targetUser.id === user.id) {
             const Embed = new EmbedBuilder()
                 .setColor('Red')
-                .setDescription(`You can't bet against yourself`);
+                .setDescription(messages.UserNoSelf);
             return await interaction.reply({ embeds: [Embed], ephemeral: true });
         }
 
@@ -118,13 +168,13 @@ module.exports = {
         if(amount > targetCurrency.cash) {
             const Embed = new EmbedBuilder()
                 .setColor('Red')
-                .setDescription(`The target user doesn't have enough cash`);
+                .setDescription(messages.TargetNotEnoughCash);
             return await interaction.reply({ embeds: [Embed], ephemeral: true });
         }
 
         const StartGameEmbed = new EmbedBuilder()
             .setColor('Blurple')
-            .setDescription(`${user.username} has challenged ${targetUser.username} to a coinflip for ${amount.toLocaleString()} cash!`);
+            .setDescription(messages.UserChallenge.replace('{user}', user.username).replace('{target}', targetUser.username).replace('{amount}', amount.toLocaleString()));
 
         
         const Buttons = choices.map(choice => {
@@ -142,16 +192,10 @@ module.exports = {
         const targetUserInteraction = await reply.awaitMessageComponent({ filter: i => i.user.id === targetUser.id, time: 60_000 }).catch(async () => {
             const Embed = new EmbedBuilder()
                 .setColor('Red')
-                .setDescription(`Game over, ${targetUser} did not respond in time`);
+                .setDescription(messages.GameOver.replace('{user}', targetUser));
                 await reply.edit({ embeds: [Embed], components: [] }).catch(() => {});
             return
-        }).catch(async () => {
-            const Embed = new EmbedBuilder()
-                .setColor('Red')
-                .setDescription(`Game over, ${targetUser} did not respond in time`);
-                await reply.edit({ embeds: [Embed], components: [] }).catch(() => {});
-            return
-        });
+        })
 
         if(!targetUserInteraction) return;
 
@@ -161,35 +205,28 @@ module.exports = {
         const randomNumber = Math.floor(Math.random() * (2 - 0) + 0);
         const sides = ['Heads', 'Tails'];
 
+        const FinishEmbed = new EmbedBuilder()
+            .setColor('Blurple');
+
         if(sides[randomNumber] === userChoice.name) {
                 
-                userCurrency.cash += amount;
-                targetCurrency.cash -= amount;
-                await userCurrency.save();
-                await targetCurrency.save();
-    
-                const Embed = new EmbedBuilder()
-                    .setColor('Blurple')
-                    .setDescription(`User: @${user.username} : ${userChoice.name} ${userChoice.emoji}\nTarget: @${targetUser.username} : ${targetUserChoice.name} ${targetUserChoice.emoji} \n\n${user.username} won ${amount} cash!`);
-                await reply.edit({ embeds: [Embed], components: [] }).catch(() => {});
-            } else {
-    
-                userCurrency.cash -= amount;
-                targetCurrency.cash += amount;
-                await userCurrency.save();
-                await targetCurrency.save();
-    
-                const Embed = new EmbedBuilder()
-                    .setColor('Red')
-                    .setDescription(`User: @${user.username} : ${userChoice.name} ${userChoice.emoji}\nTarget: @${targetUser.username} : ${targetUserChoice.name} ${targetUserChoice.emoji} \n\n${targetUser.username} won ${amount} cash!`);
-                await reply.edit({ embeds: [Embed], components: [] }).catch(() => {});
-            }
+            userCurrency.cash += amount;
+            targetCurrency.cash -= amount;
+            await userCurrency.save();
+            await targetCurrency.save();
+
+            FinishEmbed.setDescription(messages.UserWin.replace('{winner}', `@${member.user.username}`).replace('{amount}', amount.toLocaleString()).replace('{choice}', `@${member.user.username} : ${userChoice.name} ${userChoice.emoji}`).replace('{targetChoice}',  `@${targetUser.username} : ${targetUserChoice.name} ${targetUserChoice.emoji}`));
+        } else {
+
+            userCurrency.cash -= amount;
+            targetCurrency.cash += amount;
+            await userCurrency.save();
+            await targetCurrency.save();
+
+            FinishEmbed.setDescription(messages.UserWin.replace('{winner}', `@${targetUser.username}`).replace('{amount}', amount.toLocaleString()).replace('{choice}', `@${member.user.username} : ${userChoice.name} ${userChoice.emoji}`).replace('{targetChoice}',  `@${targetUser.username} : ${targetUserChoice.name} ${targetUserChoice.emoji}`));
+        }
         
-
-        // await reply.edit({ embeds: [StartGameEmbed], components: [] }).catch(() => {});
-
-
-
+        await reply.edit({ content: '', embeds: [FinishEmbed], components: [] }).catch(() => {});
 
     }
 
